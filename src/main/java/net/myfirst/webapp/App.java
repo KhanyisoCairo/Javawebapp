f6931d3ed67206846b7a1fef32172d8402a87cb5
KhanyisoCairo <Khanyisoyezo12@gmail.com>
2020-08-06 14:08:52 +0200

    herok port

--- src/main/java/net/myfirst/webapp/App.java
+++ src/main/java/net/myfirst/webapp/App.java
@@ -13,7 +13,7 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         //  port(8080);
-
+        port(getHerokuAssignedPort());
         List<String> allusers = new ArrayList<>();
         staticFiles.location("/public");
         get("/greet", (req, res) -> "Molo!");
@@ -64,4 +64,13 @@ public class App {
         }, new HandlebarsTemplateEngine());
 
     }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
+
